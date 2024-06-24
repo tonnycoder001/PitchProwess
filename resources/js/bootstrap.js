3e54265c7eed33c23f9ec49363b414b2d5b4cdca
tonnycoder001 <tonnykahihia@gmail.com>
2024-06-24 14:05:34 +0300

    Installed bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,9 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) {}
