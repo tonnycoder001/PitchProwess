a5c97a59c5938ec02bce58f02c75d74e20015844
tonnycoder001 <tonnykahihia@gmail.com>
2024-08-07 10:23:48 +0300

    Fix footer at the bottom of the page

--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,15 +1,19 @@
 @include('layouts.nav')
 <script src="https://cdn.tailwindcss.com"></script>
-<div class="border-t border-gray-800 my-6"></div>
 
-<div class="container mx-auto py-8 relative">
-    <h1 class="text-5xl font-bold text-center uppercase">{{ $news->title }}</h1>
-    <div class="max-w-3xl mx-auto px-4 pt-4">
-        <p class="text-gray-700 mt-4">{{ $news->body }}</p>
+<div class="flex flex-col min-h-screen">
+    <div class="container mx-auto py-8 flex-1">
+        <h1 class="text-5xl font-bold text-center uppercase">{{ $news->title }}</h1>
+        <div class="max-w-3xl mx-auto px-4 pt-4">
+            <p class="text-gray-700 mt-4">{{ $news->body }}</p>
+        </div>
+        <div class="text-sm text-blue-500 italic mt-4 max-w-3xl mx-auto px-4 pb-6">
+            <span>by {{ $news->author }}</span> |
+            <span>Published on {{ $news->created_at->format('F j, Y, g:i a') }}</span>
+        </div>
     </div>
-    <div class="text-sm text-blue-500 italic mt-4 max-w-3xl mx-auto px-4 pb-6">
-        <span>by {{ $news->author }}</span> |
-        <span>Published on {{ $news->created_at->format('F j, Y, g:i a') }}</span>
+
+    <div class="border-t border-gray-800 mt-6">
+        @include('layouts.footer')
     </div>
 </div>
-@include('layouts.footer')
