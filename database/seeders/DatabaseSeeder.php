d5b9d75c57d2dc3fbb61d4c07d9ce35cadbec650
tonnycoder001 <tonnykahihia@gmail.com>
2024-08-14 10:20:19 +0300

    Assign role and permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,16 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\News;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Women;
-use App\Models\Result;
-use App\Models\Senior;
 use App\Models\Fixture;
-use App\Models\Tournament;
-use App\Models\AcademyPlayer;
-use App\Models\Matches;
 use App\Models\Player;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,9 +17,20 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
+        /**
+         * var App\model\User $adminUser
+         */
+        $adminUser = User::factory()->create([
+            'email' => 'admin@gmail.com',
+            'name' => 'Admin',
+            'password' => bcrypt('password'),
+        ]);
 
+        $adminRole = Role::create([
+            'name' => 'admin'
+        ]);
+        $adminUser->assignRole($adminRole);
 
-        Fixture::factory()->count(15)->create();
         Fixture::factory()->count(15)->create();
         News::factory()->count(15)->create();
         Player::factory()->count(15)->create();
