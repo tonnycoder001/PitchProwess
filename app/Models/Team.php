a305cb2c7ae7e2638b86d1643044f4c37b19bf62
tonnycoder001 <tonnykahihia@gmail.com>
2024-08-28 15:46:39 +0300

    Add relationship

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Fixture;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Team extends Model
 {
@@ -13,4 +14,13 @@ class Team extends Model
         'name',
         'slug'
     ];
+    public function homeMatches()
+    {
+        return $this->hasMany(Fixture::class, 'home_team_id');
+    }
+
+    public function awayMatches()
+    {
+        return $this->hasMany(Fixture::class, 'away_team_id');
+    }
 }
