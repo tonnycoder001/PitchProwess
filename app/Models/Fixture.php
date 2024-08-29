4cc1539080c95e8ee476b41560799030287c486a
tonnycoder001 <tonnykahihia@gmail.com>
2024-08-29 11:56:03 +0300

    Import class standing

--- app/Models/Fixture.php
+++ app/Models/Fixture.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Team;
 use App\Enums\MatchType;
+use App\Models\Standing;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
