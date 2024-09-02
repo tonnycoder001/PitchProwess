e0f5df5f5036a51b9416721041cda0cbbaa9f54b
tonnycoder001 <tonnykahihia@gmail.com>
2024-09-02 11:13:47 +0300

    Wrap team under content

--- app/Filament/Resources/TeamResource.php
+++ app/Filament/Resources/TeamResource.php
@@ -20,6 +20,8 @@ class TeamResource extends Resource
     protected static ?string $model = Team::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Content';
+
 
     public static function form(Form $form): Form
     {
