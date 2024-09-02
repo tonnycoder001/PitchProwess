502db627a8874fd116f638fe14a0cfb8c3be0f6e
tonnycoder001 <tonnykahihia@gmail.com>
2024-09-02 14:28:54 +0300

    Restructure fixture controller from hard coded team type and introduce enum team type

--- app/Http/Controllers/FixtureController.php
+++ app/Http/Controllers/FixtureController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\TeamType;
 use App\Models\Fixture;
 use App\Enums\MatchType;
 use Illuminate\Http\Request;
@@ -27,7 +28,7 @@ class FixtureController extends Controller
 
     public function league(Request $request)
     {
-        $team = $request->input('team', 'men');
+        $team = $request->input('team', TeamType::MEN);
         $fixtures = Fixture::where('type', MatchType::LEAGUE->value)
             ->where('team', $team)
             ->get();
@@ -36,9 +37,7 @@ class FixtureController extends Controller
 
     public function tournament(Request $request)
     {
-
-
-        $team = $request->input('team', 'men');
+        $team = $request->input('team', TeamType::MEN);
         $fixtures = Fixture::where('type', MatchType::TOURNAMENT->value)
             ->where('team', $team)
             ->get();
@@ -47,52 +46,66 @@ class FixtureController extends Controller
 
     public function friendly(Request $request)
     {
-        $team = $request->input('team', 'men');
-        $fixtures = Fixture::where('type', 'friendly')->where('team', $team)->get();
-        return view('fixtures.men_friendly', compact('fixtures', 'team'));
+        $team = $request->input('team', TeamType::MEN);
+        $fixtures = Fixture::where('type', MatchType::FRIENDLY->value)
+            ->where('team', $team)
+            ->get();
+        return view('fixtures.men_tournament', compact('fixtures', 'team'));
     }
 
 
     public function women_league(Request $request)
     {
-        $team = $request->input('team', 'women');
-        $fixtures = Fixture::where('type', 'league')->where('team', $team)->get();
+        $team = $request->input('team', TeamType::WOMEN);
+        $fixtures = Fixture::where('type', MatchType::LEAGUE->value)
+            ->where('team', $team)
+            ->get();
         return view('fixtures.women_league', compact('fixtures', 'team'));
     }
 
     public function women_tournament(Request $request)
     {
-        $team = $request->input('team', 'women');
-        $fixtures = Fixture::where('type', 'tournament')->where('team', $team)->get();
+        $team = $request->input('team', TeamType::WOMEN);
+        $fixtures = Fixture::where('type', MatchType::TOURNAMENT->value)
+            ->where('team', $team)
+            ->get();
         return view('fixtures.women_tournament', compact('fixtures', 'team'));
     }
 
     public function women_friendly(Request $request)
     {
-        $team = $request->input('team', 'women');
-        $fixtures = Fixture::where('type', 'friendly')->where('team', $team)->get();
+        $team = $request->input('team', TeamType::WOMEN);
+        $fixtures = Fixture::where('type', MatchType::FRIENDLY->value)
+            ->where('team', $team)
+            ->get();
         return view('fixtures.women_friendly', compact('fixtures', 'team'));
     }
 
 
     public function academy_league(Request $request)
     {
-        $team = $request->input('team', 'academy');
-        $fixtures = Fixture::where('type', 'league')->where('team', $team)->get();
+        $team = $request->input('team', TeamType::ACADEMY);
+        $fixtures = Fixture::where('type', MatchType::LEAGUE->value)
+            ->where('team', $team)
+            ->get();
         return view('fixtures.academy_league', compact('fixtures', 'team'));
     }
 
     public function academy_tournament(Request $request)
     {
-        $team = $request->input('team', 'academy');
-        $fixtures = Fixture::where('type', 'tournament')->where('team', $team)->get();
+        $team = $request->input('team', TeamType::ACADEMY);
+        $fixtures = Fixture::where('type', MatchType::TOURNAMENT->value)
+            ->where('team', $team)
+            ->get();
         return view('fixtures.academy_tournament', compact('fixtures', 'team'));
     }
 
     public function academy_friendly(Request $request)
     {
-        $team = $request->input('team', 'academy');
-        $fixtures = Fixture::where('type', 'friendly')->where('team', $team)->get();
+        $team = $request->input('team', TeamType::ACADEMY);
+        $fixtures = Fixture::where('type', MatchType::FRIENDLY->value)
+            ->where('team', $team)
+            ->get();
         return view('fixtures.academy_friendly', compact('fixtures', 'team'));
     }
 }
