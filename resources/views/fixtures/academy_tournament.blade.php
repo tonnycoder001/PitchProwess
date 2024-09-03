90f1bf5128b63092b9bcd716e0b139fda8eca4c6
tonnycoder001 <tonnykahihia@gmail.com>
2024-09-03 12:05:54 +0300

    Add else if statement to tell users if there are no matches

--- resources/views/fixtures/academy_tournament.blade.php
+++ resources/views/fixtures/academy_tournament.blade.php
@@ -30,28 +30,32 @@
     </nav>
 
     <div class="container mx-auto px-4 pb-12 flex-grow">
-        <h1 class="text-2xl font-bold text-center my-6">{{ ucfirst($team) }} Tournament Fixtures</h1>
-        <div class="flex item-center justify-center">
-            <div class="w-full md:w-1/2 lg:w-1/2">
-                <div class="grid grid-cols-1 gap-8">
-                    @foreach ($fixtures as $fixture)
-                        <div class="relative bg-white shadow-lg rounded-lg overflow-hidden p-8">
-                            <p class="mb-2 text-red-500">{{ $fixture->name }}</p>
-                            <div class="flex justify-between items-center mb-1">
-                                <p class="text-gray-600 text-sm">{{ $fixture->date }} | <span
-                                        class="text-red-500">Tournament</span></p>
+        @if ($fixtures->isEmpty())
+            <p class="text-center mt-6 font-bold text-2xl">No Tournaments found</p>
+        @else
+            <h1 class="text-2xl font-bold text-center my-6">{{ ucfirst($team) }} Tournament Fixtures</h1>
+            <div class="flex item-center justify-center">
+                <div class="w-full md:w-1/2 lg:w-1/2">
+                    <div class="grid grid-cols-1 gap-8">
+                        @foreach ($fixtures as $fixture)
+                            <div class="relative bg-white shadow-lg rounded-lg overflow-hidden p-8">
+                                <p class="mb-2 text-red-500">{{ $fixture->name }}</p>
+                                <div class="flex justify-between items-center mb-1">
+                                    <p class="text-gray-600 text-sm">{{ $fixture->date }} | <span
+                                            class="text-red-500">Tournament</span></p>
+                                </div>
+                                <div class="flex justify-between items-center">
+                                    <h5 class="text-base font-bold">{{ $fixture->homeTeam->name }}</h5>
+                                    <p class="text-gray-600 text-xs">{{ $fixture->time }}</p>
+                                    <h5 class="text-base font-bold">{{ $fixture->awayTeam->name }}</h5>
+                                </div>
+                                <p class="text-gray-600 text-xs mt-1">{{ $fixture->location }}</p>
                             </div>
-                            <div class="flex justify-between items-center">
-                                <h5 class="text-base font-bold">{{ $fixture->homeTeam->name }}</h5>
-                                <p class="text-gray-600 text-xs">{{ $fixture->time }}</p>
-                                <h5 class="text-base font-bold">{{ $fixture->awayTeam->name }}</h5>
-                            </div>
-                            <p class="text-gray-600 text-xs mt-1">{{ $fixture->location }}</p>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </div>
+                        @endforeach
+        @endif
+    </div>
+    </div>
+    </div>
     </div>
     </div>
     </div>
