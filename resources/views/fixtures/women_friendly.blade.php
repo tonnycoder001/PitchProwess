5308f9c46138f4ed77974702657e8bd6bfb7407e
tonnycoder001 <tonnykahihia@gmail.com>
2024-09-03 12:05:35 +0300

    Add else if statement to tell users if there are no matches

--- resources/views/fixtures/women_friendly.blade.php
+++ resources/views/fixtures/women_friendly.blade.php
@@ -13,49 +13,54 @@
 
     <div class="border-t border-gray-300"></div>
 
-    <div class="flex-1 flex justify-center">
-        <nav class=" p-4">
-            <div class="container mx-auto flex justify-center items-center">
-                <ul class="flex space-x-4">
-                    <a href="{{ route('fixtures.women_league', ['team' => 'women']) }}"
-                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Women's
-                        League</a>
-                    <a href="{{ route('fixtures.women_tournament', ['team' => 'women']) }}"
-                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">women's
-                        Tournament</a>
-                    <a href="{{ route('fixtures.women_friendly', ['team' => 'women']) }}"
-                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hidden md:flex"
-                        role="menuitem">women's
-                        Friendly</a>
-                </ul>
-            </div>
-        </nav>
+    <nav class=" p-4">
+        <div class=" mx-auto flex justify-center items-center">
+            <ul class="flex space-x-4">
+                <a href="{{ route('fixtures.women_league', ['team' => 'women']) }}"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100  hidden md:flex"
+                    role="menuitem">Women's
+                    League</a>
+                <a href="{{ route('fixtures.women_tournament', ['team' => 'women']) }}"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">women's
+                    Tournament</a>
+                <a href="{{ route('fixtures.women_friendly', ['team' => 'women']) }}"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hidden md:flex"
+                    role="menuitem">women's
+                    Friendly</a>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mx-auto px-4 pb-12 flex-grow">
+        @if ($fixtures->isEmpty())
+            <p class="text-center mt-6 font-bold text-2xl">No Friendlies found</p>
+        @else
+            <h1 class="text-2xl font-bold text-center my-6">{{ ucfirst($team) }} Friendly Fixtures</h1>
+            <div class="flex item-center justify-center">
+                <div class="w-full md:w-1/2 lg:w-1/2">
+                    <div class="grid grid-cols-1 gap-8">
+                        @foreach ($fixtures as $fixture)
+                            <div class="relative bg-white shadow-lg rounded-lg overflow-hidden p-8">
+                                <p class="mb-2 text-red-500">{{ $fixture->name }}</p>
+                                <div class="flex justify-between items-center mb-1  ">
+                                    <p class="text-gray-600 text-sm">{{ $fixture->date }} | <span
+                                            class="text-red-500">Friendly</span></p>
+                                </div>
+                                <div class="flex justify-between items-center">
+                                    <h5 class="text-base font-bold">{{ $fixture->homeTeam->name }}</h5>
+                                    <p class="text-gray-600 text-xs">{{ $fixture->time }}</p>
+                                    <h5 class="text-base font-bold">{{ $fixture->awayTeam->name }}</h5>
+                                </div>
+                                <p class="text-gray-600 text-xs mt-1">{{ $fixture->location }}</p>
+                            </div>
+                        @endforeach
+        @endif
     </div>
 
-    <div class="container mx-auto px-4 pb-12">
-        <h1 class="text-2xl font-bold text-center my-6">{{ ucfirst($team) }} Friendly Fixtures</h1>
-        <div class="flex item-center justify-center">
-            <div class="w-full md:w-1/2 lg:w-1/2">
-                <div class="grid grid-cols-1 gap-8">
-                    @foreach ($fixtures as $fixture)
-                        <div class="relative bg-white shadow-lg rounded-lg overflow-hidden p-8">
-                            <p class="mb-2 text-red-500">{{ $fixture->name }}</p>
-                            <div class="flex justify-between items-center mb-1  ">
-                                <p class="text-gray-600 text-sm">{{ $fixture->date }} | <span
-                                        class="text-red-500">Friendly</span></p>
-                            </div>
-                            <div class="flex justify-between items-center">
-                                <h5 class="text-base font-bold">{{ $fixture->homeTeam->name }}</h5>
-                                <p class="text-gray-600 text-xs">{{ $fixture->time }}</p>
-                                <h5 class="text-base font-bold">{{ $fixture->awayTeam->name }}</h5>
-                            </div>
-                            <p class="text-gray-600 text-xs mt-1">{{ $fixture->location }}</p>
-                        </div>
-                    @endforeach
-                </div>
-            </div>
+    </div>
 
-        </div>
+
+    </div>
     </div>
 
     @include('layouts.footer')
